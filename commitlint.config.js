4eeaa708e5b9e239fa758a260b284fa8878b2a10
Milos Curuvija <curuvija@live.com>
2024-06-23 04:56:14 +0200

    chore: commitlint update scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,8 @@ module.exports = {
         "@commitlint/config-conventional"
     ],
     rules: {
+        // find meaning and explanations here https://github.com/angular/angular/blob/22b96b9/CONTRIBUTING.md#type
         "type-enum": [2, "always", ["build", "chore", "ci", "docs", "feat", "fix", "refactor", "revert", "style", "test"]],
-        "scope-enum": [2, "always", ["chart", "test"]]
+        "scope-enum": [2, "always", ["chart", "test", "taskfile"]]
     }
 }
