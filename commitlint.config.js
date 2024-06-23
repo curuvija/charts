76bfff6d564983272379077fa7e72d25eecb4c1e
Milos Curuvija <curuvija@live.com>
2024-06-23 05:04:30 +0200

    feat: update docker image to version 0.6.0

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,6 +5,6 @@ module.exports = {
     rules: {
         // find meaning and explanations here https://github.com/angular/angular/blob/22b96b9/CONTRIBUTING.md#type
         "type-enum": [2, "always", ["build", "chore", "ci", "docs", "feat", "fix", "refactor", "revert", "style", "test"]],
-        "scope-enum": [2, "always", ["chart", "test", "taskfile"]]
+        "scope-enum": [2, "always", ["query-exporter", "oracledb-exporter", "data-prepper"]]
     }
 }
